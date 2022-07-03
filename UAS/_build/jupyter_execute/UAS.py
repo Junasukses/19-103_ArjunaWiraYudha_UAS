7caa05622e2206c28e42e6bc6d187d29568f18c4
190411100103 <wxarjuna@gmail.com>
2022-07-03 14:48:37 +0700

    third commit

--- UAS/_build/jupyter_execute/UAS.py
+++ UAS/_build/jupyter_execute/UAS.py
@@ -44,7 +44,7 @@ import re
 #     <li>Mereplace kata yang tidak digunakan dengan kata yang ingin digunakan</li>
 # </ul>
 
-# In[202]:
+# In[4]:
 
 
 class QuotesSpider(scrapy.Spider):
@@ -87,13 +87,13 @@ class QuotesSpider(scrapy.Spider):
 
 # Library yang perlu diinstall untuk melakukan preprocesing data ada dua yaitu, nltk dan scikit learn
 
-# In[203]:
+# In[5]:
 
 
 pip install --user -U nltk
 
 
-# In[204]:
+# In[6]:
 
 
 pip install -U scikit-learn
@@ -103,7 +103,7 @@ pip install -U scikit-learn
 
 # Sesudah install, kita import library yang dibutuhkan (untuk string udah otomatis terinstall)
 
-# In[205]:
+# In[7]:
 
 
 import pandas as pd
@@ -120,7 +120,7 @@ import re
 
 # Stop Word adalah tahap untuk menghilangkan kata yang tidak memiliki arti, seperti preposisi, konjungsi, dan lain sebagainya. Contoh kata yang dihilangkan dari Stop Word adalah yang, di, ke, dan lainnya. Tanpa perlu berlama-lama mari langsung kepada tahap kodingnya, pertama tama kita load data yang sudah kita crawling tadi. Karena tadi hasil yang saya save dengan nama **crawlingpta.csv** maka pada saat load dengan pandas yang saya tuju adalah file **crawlingpta.csv**
 
-# In[206]:
+# In[8]:
 
 
 jurnal = pd.read_csv('crawlUAS.csv')
@@ -128,7 +128,7 @@ jurnal = pd.read_csv('crawlUAS.csv')
 
 # Sesudah meload data selanjutnya memilih kolom yang ingin di proses, disini saya akan memproses kolom **abstraksi**, dan pada kolom itu juga saya akan menghilangkan angka yang akan mengganggu. Tahap ini juga termasuk dalam bagian Cleaning Data, tahap ini saya lakukan di awal karena kalau udah masuk ke stop word akan susah di proses. Untuk melakukannya saya buat function yang bernama **remove_number** dan di function ini akan mengembalikan nilai berupa text dimana jika ada angka akan dihapus, dan ketika memanggil kolom dikasih apply dan memanggil functionnya
 
-# In[207]:
+# In[9]:
 
 
 def remove_number(text):
@@ -140,7 +140,7 @@ pre_abstrak
 
 # Kemudian langkah sebelum memasuki stop word adalah harus tokenize kalimat dahulu, tokenize adalah proses untuk membagi kalimat ke dalam bagian bagian tertentu
 
-# In[208]:
+# In[10]:
 
 
 word_tokens = pre_abstrak.apply(word_tokenize)
@@ -149,7 +149,7 @@ word_tokens
 
 # Langkah selanjutnya adalah Stop Word. Karena disini saya menggunakan nltk maka harus menentukan dahulu bahasa yang digunakan untuk menentukan bahasa menggunakan **stopwords.words('indonesian')**. Kemudian jika dirasa list stop word masih ada yang kurang maka kita bisa menambahkan sendiri dengan cara membuat list kata yang tidak ada di stop word kemudian kita extend dengan list yang kita buat sendiri **stop_words.extend(list)**
 
-# In[209]:
+# In[11]:
 
 
 stop_words = stopwords.words('indonesian')
@@ -166,7 +166,7 @@ for i in range(1,4):
 
 # Cleaning Data adalah proses untuk membersihkan data yang ada menjadi data yang bisa diolah. Data yang dibersihkan seperti missing value atau data kosong, karakter asing, menghilangkan angka, dan lain sebaginaya. Untuk proses penghilangan angka sudah dilakukan ketika memilih tabel **abstraksi**, maka sekarang tinggal menghilangkan karakter asing dan sekawannya. Untuk melakukan itu kita bisa menggunakan library string.punctuation. Dimana ia akan menghilangkan karakter asing yang ada
 
-# In[210]:
+# In[12]:
 
 
 clearData = [[w for w in z if w not in string.punctuation and w.isalpha()] for z in after] 
@@ -187,7 +187,7 @@ for i in range(1,4):
 # </ul>
 # Untuk menggunakan Tf-IDF pada python kita bisa menggunakan TfidfVectorizer dari sklearn
 
-# In[211]:
+# In[13]:
 
 
 vectorizer = TfidfVectorizer(stop_words='english')
@@ -208,7 +208,6 @@ tfidf=TfidfTransformer(use_idf=True, norm='l2', smooth_idf=True)
 tf = tfidf.fit_transform(vectorizer.fit_transform(berita)).toarray()
 
 dfp =pd.DataFrame(data=tf, index=range(1,len(tf[:,1])+1, ),columns=[a])
-dfp
 
 
 # # K-Means Clustering
@@ -225,25 +224,25 @@ dfp
 
 # Sebelum memasuki koding kita perlu menginstall beberapa library seperti : scikit-learn, numpy, seaborn, matplotlib
 
-# In[212]:
+# In[14]:
 
 
 pip install -U scikit-learn
 
 
-# In[213]:
+# In[15]:
 
 
 pip install numpy
 
 
-# In[214]:
+# In[16]:
 
 
 pip install seaborn
 
 
-# In[215]:
+# In[17]:
 
 
 pip install matplotlib
@@ -253,7 +252,7 @@ pip install matplotlib
 
 # Import library yang dibutuhkan seperti, KMean dari sklearn.cluster, PCA dari sklearn.decomposition, numpy, seaborn
 
-# In[216]:
+# In[18]:
 
 
 from sklearn.cluster import KMeans
@@ -263,22 +262,39 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
+# ## Reduksi Dimensi
+
+# Sebelum masuk ke metode KMeans kita perlu mereduksi dimensi dahulu, untuk melakukan reduksi dimensi kita bisa menggunakan teknik Principal Component Analysis (PCA), dimana PCA akan mengurangi dimensi kumpulan data menjadi angka arbitrary sambil mempertahankan informasi yang ada pada data. Sederhananya PCA akan cenderung mempertahankan dimensi yang paling baik, dengan menghapus dimensi yang kontribusinya sedikit pada dimensi terakhir
+
+# In[19]:
+
+
+pca = PCA(n_components=2, random_state=42)
+pca_vecs = pca.fit_transform(dfp)
+x0 = pca_vecs[:, 0]
+x1 = pca_vecs[:, 1]
+print(x0)
+print(x1)
+
+
+# Pada code di atas pertama kita inisalisasi PCA dahulu dengan 2 komponen, kemudian set PCA dengan vector dari tfidf dan disimpan vector yang direduksi pada variabel pca_vecs. Kemudian simpan 2 dimensi tadi kepada variabel x0 dan x1. elanjutnya kita tambahkan kolom cluster, x0, dan x1 pada dataframe jurnal kita 
+
 # ## Menentukan Nilai K Terbaik Dengan Metode Elbow
 
 # Langkah awal untuk memulai KMeans adalah dengan menentukan berapa banyak cluster yang mau ditentukan, untuk mencari cluster berapa banyak cluster yang tepat bisa menggunakan metode elbow. Metode Elbow sendiri merupakan salah satu metode untuk menentukan jumlah cluster yang tepat pada K-Means melalui persentase hasil perbandingan antara jumlah cluster yang akan membentuk siku siku pada suatu titik cluster yang tepat. Untuk mencari Elbow bisa menggunakan looping dengan ketentuan cluster pada insialisasi KMeans bernilai iterasi loopingnya, berikut adalah code dan visualisasi elbownya
 
-# In[217]:
+# In[20]:
 
 
 distortions = []
 K = range(1,10)
 for k in K:
     kmeanModel = KMeans(n_clusters=k)
-    kmeanModel.fit(df_pca)
+    kmeanModel.fit(pca_vecs)
     distortions.append(kmeanModel.inertia_)
 
 
-# In[218]:
+# In[21]:
 
 
 plt.figure(figsize=(10,8))
@@ -289,39 +305,26 @@ plt.title('Nilai k yang paling optimal adalah')
 plt.show()
 
 
-# Dapat dilihat bahwa K yang membentuk siku adalah K yang kelima maka nanti pada implementasi K-Means akan menggunakan 5 cluster
+# Dapat dilihat bahwa K yang membentuk siku adalah K yang ketiga maka nanti pada implementasi K-Means akan menggunakan 3 cluster
 
 # ## Inisialisasi K-Means
 
 # Sesudah import library, selanjutnya inisasi KMean dengan 3 centroid atau cluster sesuai dengan hasil metode elbow. Kemudian fit dengan vector dari tfidf
 
-# In[219]:
+# In[22]:
 
 
-kmeans = KMeans(n_clusters=5, random_state=42)
+kmeans = KMeans(n_clusters=3, random_state=42)
 # fit the model
 kmeans.fit(dfp)
 clusters = kmeans.labels_
 
 
-# ## Reduksi Dimensi
+# ## Labeling Cluster
 
-# Untuk melakukan reduksi dimensi kita bisa menggunakan teknik Principal Component Analysis (PCA), dimana PCA akan mengurangi dimensi kumpulan data menjadi angka arbitrary sambil mempertahankan informasi yang ada pada data. Sederhananya PCA akan cenderung mempertahankan dimensi yang paling baik, dengan menghapus dimensi yang kontribusinya sedikit pada dimensi terakhir
+# Sebelum melakukan labeling, pertama kita tambahkan dahulu kolom cluster dan hasil reduksi dimensi yang telah dilakukan
 
-# In[220]:
-
-
-pca = PCA(n_components=2, random_state=42)
-pca_vecs = pca.fit_transform(vect_text.toarray())
-x0 = pca_vecs[:, 0]
-x1 = pca_vecs[:, 1]
-print(x0)
-print(x1)
-
-
-# Pada code di atas pertama kita inisalisasi PCA dahulu dengan 2 komponen, kemudian set PCA dengan vector dari tfidf dan disimpan vector yang direduksi pada variabel pca_vecs. Kemudian simpan 2 dimensi tadi kepada variabel x0 dan x1. elanjutnya kita tambahkan kolom cluster, x0, dan x1 pada dataframe jurnal kita 
-
-# In[221]:
+# In[23]:
 
 
 jurnal['cluster'] = clusters
@@ -330,14 +333,12 @@ jurnal['x1'] = x1
 jurnal[['Abstraksi','cluster','x0','x1']].head(5)
 
 
-# ## Labeling Cluster
+# Kemudian kita bisa melabeling dengan menggunakan logika mapping dan nilai value disini kita cocokan saja dengan nilai keynya
 
-# Untuk melakukan labeling cluster, bisa menggunakan logika mapping dan nilai value disini kita cocokan saja dengan nilai keynya
-
-# In[222]:
+# In[24]:
 
 
-cluster_map = {0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5:5}
+cluster_map = {0: 0, 1: 1, 2: 2, 3: 3}
 jurnal['cluster'] = jurnal['cluster'].map(cluster_map)
 jurnal[['Abstraksi','cluster','x0','x1']].head(5)
 
@@ -346,7 +347,7 @@ jurnal[['Abstraksi','cluster','x0','x1']].head(5)
 
 # Untuk memvisualisasikan kita menggunakan library seaborn karena caranya cukuplah mudah, berikut adalah cara menggunakan seaborn yang disimpan kedalam kata sns
 
-# In[236]:
+# In[25]:
 
 
 plt.figure(figsize=(10, 5))
@@ -357,7 +358,7 @@ sns.scatterplot(data=jurnal, x='x0', y='x1', hue='cluster', palette="viridis")
 plt.show()
 
 
-# Dapat dilihat bahwa sistem mendeteksi 5 kelompok yang berbeda yang didapatkan dari kolom cluster pada data
+# Dapat dilihat bahwa sistem mendeteksi 3 kelompok yang berbeda yang didapatkan dari kolom cluster pada data
 
 # # Topik Modeling LSA
 
@@ -365,7 +366,7 @@ plt.show()
 
 # ## Install Library
 
-# In[224]:
+# In[26]:
 
 
 pip install -U scikit-learn
@@ -373,7 +374,7 @@ pip install -U scikit-learn
 
 # ## Import Library
 
-# In[225]:
+# In[27]:
 
 
 from sklearn.decomposition import TruncatedSVD
@@ -392,7 +393,7 @@ from sklearn.decomposition import TruncatedSVD
 # </ol>
 # Setelah mengetahui poin penting, berikut code untuk cari Tf-IDF
 
-# In[226]:
+# In[28]:
 
 
 def dummy(doc):
@@ -405,7 +406,7 @@ df = pd.DataFrame(vect_text_tranpose.toarray())
 
 # Setelah kita set Tf-idf dari data, kita cek dahulu apakah baris dan kolom sesuai dengan kata dan dokumennya menggunakan fungsi **shape** dan kita coba print 5 nilai
 
-# In[227]:
+# In[29]:
 
 
 print(vect_text.shape)
@@ -414,7 +415,7 @@ print(df.head(5))
 
 # Setelah Tf-idf berhasil dilakukan, kita bisa melihat kata yang paling sering muncul dan kata yang paling jarang muncul berdasarkan nilai Tf-idf diatas. Semakin kecil nilainya maka dia sering digunakan, sedangkan jika besar nilainya maka dia jarang digunakan.
 
-# In[228]:
+# In[30]:
 
 
 idf=vect.idf_
@@ -446,7 +447,7 @@ print(dd['with'])
 
 # ![Visualisasi Truncated SVD](visualisasi.png)
 
-# In[229]:
+# In[31]:
 
 
 lsa_model = TruncatedSVD(n_components=10, algorithm='randomized', n_iter=10, random_state=42)
@@ -456,7 +457,7 @@ print(lsa_top)
 print(lsa_top.shape)
 
 
-# In[230]:
+# In[32]:
 
 
 l=lsa_top[0]
@@ -465,7 +466,7 @@ for i,topic in enumerate(l):
   print("Topic ",i," : ",topic*100)
 
 
-# In[231]:
+# In[33]:
 
 
 print(lsa_model.components_.shape)
@@ -476,7 +477,7 @@ print(lsa_model.components_)
 
 # Setelah dilakukan Truncated Matrix, sekarang kita dapat melakukan ekstrak topik dokumen. Pada percobaan kali ini, dilakukan extrak sebanyak 10 topik.
 
-# In[232]:
+# In[34]:
 
 
 vocab = vect.get_feature_names_out()
